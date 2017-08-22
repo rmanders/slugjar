516763a9f6c20fff0756278c4ebd753468ad4c68
rmanders <rmanders310@yahoo.com>
2017-08-21 23:27:29 -0400

    Security

---
+++ src/main/java/org/schlocknet/slugjar/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.schlocknet.slugjar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable().authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/info").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/user/login").permitAll()
+            .anyRequest().authenticated();
+    }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      // Do Nothing
+  }
+}
