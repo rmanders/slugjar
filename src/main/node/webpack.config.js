05fd6988ca5c206cafd90c8aae331d109ed8e404
rmanders <rmanders310@yahoo.com>
2018-06-02 19:52:22 -0400

    more webpack

--- src/main/node/webpack.config.js
+++ src/main/node/webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Slugjar Blog'
+    })
+  ],
   module: {
     rules: [
       {
